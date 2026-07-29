abc44858022f1a0bec25937f0b438c7a8ff3ea07
gitduck6 <warmsot19@gmail.com>
2026-07-29 15:53:48 +0400

    Added needed headers and functions to the header file

--- include/info.h
+++ include/info.h
@@ -3,11 +3,14 @@
 
 #include <sys/utsname.h>
 #include <sys/sysinfo.h>
+#include <sys/types.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <entry.h>
+#include <pwd.h>
 
+char * get_userathost()
 char * get_prettyname();
 char * get_hostname();
 char * get_kernel();
