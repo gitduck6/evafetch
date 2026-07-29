056d351e20c5b7244353a66a7295f56efe6a8073
gitduck6 <warmsot19@gmail.com>
2026-07-29 16:05:28 +0400

    Added to entry

--- src/entry.c
+++ src/entry.c
@@ -5,6 +5,7 @@ struct sysinfo Systeminfo = {0};
 struct utsname Uname = {0};
 entry entries[] =
 {
+    {.last = 0, .name = NULL, .func_ptr = &get_userathost},
     {.last = 0, .name = "OS", .func_ptr = &get_prettyname},
     {.last = 0, .name = "Hostname", .func_ptr = &get_hostname},
     {.last = 0, .name = "Kernel", .func_ptr = &get_kernel},
