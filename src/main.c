9c4568386cc66b143d450e42a433e73c3146bde6
gitduck6 <warmsot19@gmail.com>
2026-07-30 12:39:54 +0400

    Seperated main to a function

--- src/main.c
+++ src/main.c
@@ -3,23 +3,31 @@
 #include <info.h>
 #include <stdlib.h>
 
+#define LOGO "logo.txt"
+
+static inline void print_entry(entry current_entry)
+{
+    if (current_entry.name != NULL)
+        printf("%s", current_entry.name);
+    if ((current_entry.name != NULL) && (current_entry.func_ptr != NULL))
+        printf(": ");
+
+    if (current_entry.func_ptr != NULL)
+    {
+        char * value = current_entry.func_ptr();
+        printf("%s", value);
+        free(value);
+    }
+}
+
 int main(void)
 {
 
+
+
     for (int i = 0;entries[i].last == 0;i++)
     {
-
-        if (entries[i].name != NULL)
-            printf("%s", entries[i].name);
-        if ((entries[i].name != NULL) && (entries[i].func_ptr != NULL))
-            printf(": ");
-
-        if (entries[i].func_ptr != NULL)
-        {
-            char * value = entries[i].func_ptr();
-            printf("%s", value);
-            free(value);
-        }
+        print_entry(entries[i]);
 
         fputc('\n', stdout);
 
