f929c9f644c3313dd777d518fbee535dd1ac38bc
gitduck6 <warmsot19@gmail.com>
2026-08-02 11:32:27 +0400

    Added error checking

--- src/main.c
+++ src/main.c
@@ -49,6 +49,11 @@ int main(int argc, char ** argv)
 
     char logo_line[64];
     FILE * fp = fopen(logo_file, "r");
+    if (fp == NULL)
+    {
+        perror("fopen");
+        return 2;
+    }
 
     int max_len = 0;
 
